c6a84f521949616035132f7104c1e7c22d510eaf
Abdelrahman Arafat <darkinb255@gmail.com>
2021-05-12 08:30:39 +0200

    Header & Footer layouts

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <Navbar />
+  <Header />
   <router-view></router-view>
+  <Footer />
 </template>
 
 <script setup>
 import Navbar from "./common/components/Navbar.vue";
+import Header from "./common/components/Header.vue";
+import Footer from "./common/components/Footer.vue";
 </script>
 
 <style lang="scss">
---
+++ src/common/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="header"></div>
+</template>
+
+<script setup></script>
