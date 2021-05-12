83fd938f0318c0386671ca55375d9af76c494b25
Abdelrahman Arafat <darkinb255@gmail.com>
2021-05-12 10:37:39 +0200

    About Inner Routes

---
+++ src/modules/About/AboutSideBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about-sidebar">
+    <router-link to="about"><div class="option">About Us</div></router-link>
+    <router-link to="our-team"><div class="option">Our Team</div></router-link>
+    <router-link to="life-at-ketofan"
+      ><div class="option">Life At Ketofan</div></router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>About</h1>
+  <div class="about">
+    <AboutSideBar />
+    <div class="content"></div>
+  </div>
+</template>
+
+<script setup>
+import AboutSideBar from "../modules/About/AboutSideBar.vue";
+</script>
