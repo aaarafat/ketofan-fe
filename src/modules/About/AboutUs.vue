9b160768a233b408e2d1e325f5c8482759eed7cb
Abdelrahman Arafat <darkinb255@gmail.com>
2021-05-12 10:46:19 +0200

    About Us Clean

--- src/modules/About/AboutUs.vue
+++ src/modules/About/AboutUs.vue
@@ -1,24 +1,5 @@
 <template>
   <h2 class="">About Us</h2>
-  <p class="">
-    ketofan.com is the leading digital healthcare booking platform and practice
-    management software in MENA. We are pioneering the shift to automated
-    physician, clinic and hospital bookings making healthcare easily accessible
-    in the region.
-  </p>
-  <p class="">
-    With over 200,000 verified reviews, patients are able to search, compare,
-    and book the best doctors in just 1 minute. Doctors also provide Patients
-    with seamless healthcare experiences through our clinic management software.
-  </p>
-  <p class="">
-    We operate in Egypt, KSA, Jordan, Lebanon, Kenya and Nigeria. We strive to
-    lead every aspect of the healthcare industry and continue to launch products
-    that have positive impact on people’s lives.
-  </p>
-  <h3>
-    <a href="" class="">Contact Us</a>
-  </h3>
 </template>
 
 <script>
