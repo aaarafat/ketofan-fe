ca2aba529145da900bb4f93da509f2e940e520a1
Abdelrahman Arafat <darkinb255@gmail.com>
2021-05-13 22:09:44 +0200

    about sidebar

--- src/modules/About/AboutSideBar.vue
+++ src/modules/About/AboutSideBar.vue
@@ -1,15 +1,65 @@
 <template>
   <div class="about-sidebar">
-    <router-link to="about"><div class="option">About Us</div></router-link>
-    <router-link to="our-team"><div class="option">Our Team</div></router-link>
+    <router-link to="about"
+      ><div
+        class="option"
+        @click="handleSelction"
+        id="about"
+        :class="{ active: aboutSelected }"
+      >
+        <span class="material-icons"> info </span>
+        About Us
+      </div></router-link
+    >
+    <router-link to="our-team"
+      ><div
+        class="option"
+        @click="handleSelction"
+        id="our-team"
+        :class="{ active: ourTeamSelected }"
+      >
+        <span class="material-icons"> people </span>
+        Our Team
+      </div></router-link
+    >
     <router-link to="life-at-ketofan"
-      ><div class="option">Life At Ketofan</div></router-link
+      ><div
+        class="option"
+        @click="handleSelction"
+        id="life-at-ketofan"
+        :class="{ active: lifeAtKetofanSelected }"
+      >
+        <span class="material-icons"> apartment </span>
+        Life At Ketofan
+      </div></router-link
     >
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      selected: "about",
+    };
+  },
+  methods: {
+    handleSelction(e) {
+      this.selected = e.target.id;
+    },
+  },
+  computed: {
+    aboutSelected() {
+      return this.selected === "about";
+    },
+    ourTeamSelected() {
+      return this.selected === "our-team";
+    },
+    lifeAtKetofanSelected() {
+      return this.selected === "life-at-ketofan";
+    },
+  },
+};
 </script>
 
 <style></style>
