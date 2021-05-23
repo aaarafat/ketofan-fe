e2161fae66df7c722518bd915c1ac9a20007d209
Abdelrahman Arafat <darkinb255@gmail.com>
2021-05-23 02:59:55 +0200

    Home using Store DB.json filled w/ specialties

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,14 @@
 <template>
   <h1>Home</h1>
+  <h2>{{ getSpecialties }}</h2>
 </template>
 
-<script setup></script>
+<script setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+const getSpecialties = computed(() => {
+  return store.getters.getSpecialties;
+});
+</script>
