03d44c2e0a3c5d22d8045dc427142266a0baae7d
Abdelrahman Arafat <darkinb255@gmail.com>
2021-06-06 01:02:48 +0200

    Doctor Appointments

---
+++ src/modules/DoctorPanel/DoctorAppointment.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="appointment">
+    <div class="head">
+      <span class="material-icons-outlined"> calendar_today </span>
+      <span>{{ appointment.patient }}</span>
+      <span class="material-icons-outlined note"> sticky_note_2 </span>
+    </div>
+    <div class="body">
+      <div class="details">
+        <div>
+          {{ date }}
+        </div>
+        <div>
+          {{ appointment.type }}
+        </div>
+      </div>
+      <div class="options"></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, defineProps } from "vue";
+
+const props = defineProps({ appointment: Object });
+console.log(Date.toString(props.appointment.date));
+const date = computed(() => {
+  return new Date(props.appointment.date).toDateString();
+});
+</script>
+
+<style></style>
