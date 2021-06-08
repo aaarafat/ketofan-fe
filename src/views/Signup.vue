de169bb9c3e0da08c97c99fa22dc3cbf6954bd30
lido22 <ahmedwa1999@gmail.com>
2021-06-08 11:14:22 +0200

    added Gender field in sign up

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -18,29 +18,39 @@
         <div class="formBody signupFormBody">
             <div class="formField">
                 <label for="">Your Name</label>
-                <input class="loginInput" name="name" type="name" placeholder="First and Last name" v-bind:value="name">
+                <input class="loginInput" name="name" type="name" placeholder="First and Last name" v-model="data.name">
+            </div>
+
+            <div class="formField" >
+                <label class="gender" for="">Gender</label>
+                <div class="gender loginInput">
+                    <label class="radio" for="male">Male</label>
+                    <input class="radio" type="radio" id="male" name="gender" value="M" v-model="data.gender">
+                    <label class="radio" for="female">Female</label>
+                    <input class="radio" type="radio" id="female" name="gender" value="F" v-model="data.gender">
+                </div>
             </div>
             <div class="formField">
                 <label for="">Mobile Number</label>
-                <input class="loginInput" name="phone" type="number" placeholder="Mobile Number" v-bind:value="phone">
+                <input class="loginInput" name="phone" type="number" placeholder="Mobile Number" v-model="data.phone">
             </div>
             
             <div class="formField">
                 <label for="">Email</label>
-                <input class="loginInput" name="email" type="email" placeholder="example@domain.com" v-bind:value="email">
+                <input class="loginInput" name="email" type="email" placeholder="example@domain.com" v-model="data.email">
             </div>
             
             <div class="formField">
                 <label for="">
                     Birthdate
                 </label>
-                <input class="loginInput" name="Birthdate" type="date" v-bind:value="birthdate">
+                <input class="loginInput" name="Birthdate" type="date" v-model="data.birthdate">
             </div>
             <div class="formField">
                 <label for="">Password</label>
-                <input class="loginInput" name="password" type="password" placeholder="Password" v-bind:value="password">
+                <input class="loginInput" name="password" type="password" placeholder="Password" v-model="data.password">
             </div>
-            <button class='loginButton loginInput'>Join Now</button>
+            <button @click="handleSubmit" class='loginButton loginInput'>Join Now</button>
         </div>
 
         
@@ -58,18 +68,31 @@
         
 </template>
 
-<script>
-export default {
-    data(){
-        return{
-            email:"",
-            password:"",
-            phone:"",
-            birthdate:"",
-            name:""
-        }
-    }
-}
+<script setup>
+import { inject, reactive, ref } from "vue";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+const api = inject("api");
+const store = useStore();
+const router = useRouter();
+const data = reactive({ 
+    email: "", 
+    password: "",
+    gender:"",
+    phone:"",
+    birthdate:"",
+    name:"",
+});
+const handleSubmit = async () => {
+  console.log(data.male);
+  const res = await api.register.post({ ...data });
+  if (res) {
+    store.dispatch("setUser", res);
+
+    if (store.getters.isDoctor) router.push("/doctor");
+    else router.push("/");
+  }
+};
 </script>
 
 <style scoped>
